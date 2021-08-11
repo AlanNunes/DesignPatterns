e7c4f5110cffc75dc7fd445f1ad0b2fffac9844e
AlanNunes <alann.625@gmail.com>
2021-08-10 22:08:34 -0300

    Add Cache sample for Singleton Pattern

---
+++ CacheSingleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using CacheSingleton.Singleton;
+using System;
+
+namespace CacheSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Getting cache object... Cache Guid: {Cache.GetInstance().Guid}");
+            Console.WriteLine($"Getting cache object... Cache Guid: {Cache.GetInstance().Guid}");
+            Console.WriteLine($"Getting cache object... Cache Guid: {Cache.GetInstance().Guid}");
+            Console.WriteLine("The 3 times we get cache, it is the same!");
+        }
+    }
+}
---
+++ CacheSingleton/Singleton/Cache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CacheSingleton.Singleton
+{
+    public class Cache
+    {
+        private static Cache _cache;
+
+        public Guid Guid { get; set; }
+
+        public Cache() { }
+
+        public static Cache GetInstance()
+        {
+            if (_cache == null)
+            {
+                _cache = new Cache() { Guid = Guid.NewGuid() };
+            }
+            return _cache;
+        }
+    }
+}
